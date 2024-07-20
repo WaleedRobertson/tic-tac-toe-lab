db7d1e6b2bdde66ddafe9a2effcb1f8ae9a0f8c5
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-20 10:33:54 -0400

    Steps 1-2 of lab complete

---
+++ js/app.js
@@ -0,0 +1,42 @@
+//1) Define the required variables used to track the state of the game.
+let board 
+let turn
+let winner
+let tie
+//2) Store cached element references.
+const squareEls = document.querySelectorAll('.sqr')
+const messageEl = document.querySelector('#message')
+console.dir(messageEl)
+//3) Upon loading, the game state should be initialized, and a function should 
+//   be called to render this game state.
+
+//4) The state of the game should be rendered to the user.
+
+//5) Define the required constants.
+
+//6) Handle a player clicking a square with a `handleClick` function.
+
+//7) Create Reset functionality.
+
+
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
