1f9097b7dbcc1aba4d3fa79a90b129ba302737f3
Waleed Robertson <waleedrbrtsn1@email.com>
2024-07-20 12:05:33 -0400

    Step 4 of lab complete

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 //let tie
 //2) Store cached element references.
 const squareEls = document.querySelectorAll('.sqr')
-// const messageEl = document.querySelector('#message')
+const messageEl = document.querySelector('#message')
 
 //3) Upon loading, the game state should be initialized, and a function should 
 //   be called to render this game state.
@@ -27,8 +27,8 @@ const squareEls = document.querySelectorAll('.sqr')
 /*---------------------------- Variables (state) ----------------------------*/
 let board = ["", "", "", "", "", "", "", "", ""]
 // let turn = X;
-// let winner = false;
-// let tie = false;
+let winner = false;
+let tie = false;
 
 
 /*------------------------ Cached Element References ------------------------*/
@@ -40,9 +40,7 @@ let board = ["", "", "", "", "", "", "", "", ""]
 
 // }
 
-// function render() {
-//     console.log("Initialization complete");
-// }
+
 
 // c. In the updateBoard function, loop over board and for each element:
 
@@ -50,34 +48,38 @@ let board = ["", "", "", "", "", "", "", "", ""]
 // Style that square however you wish, dependent on the value contained in the current cell being 
 // iterated over ('X', 'O', or ''). To keep it simple, 
 // start by just putting a letter in each square depending on the value of each cell.
+function render() {
+    updateBoard();
+    updateMessage();
+}
 
-// function updateBoard() {
-//     board.forEach((cellValue, index) => {
-//         const square = squareEls[index];
-//         square.textContent = cellValue;
-//     })}
+render()
+
+function updateBoard() {
+    board.forEach((cellValue, index) => {
+        const square = squareEls[index];
+        square.textContent = cellValue;
+    })
+}
 
 function updateMessage() {
     const messageEl = document.getElementById('message');
 
     if (!winner && !tie) {
         // Game is still in progress
-        messageEl.textContent = `It's ${currentPlayer}'s turn.`;
+        messageEl.textContent = `It's 's turn.`;
     } else if (!winner && tie) {
         // Game is a tie
         messageEl.textContent = 'It\'s a tie!';
     } else {
         // There is a winner
-        messageEl.textContent = `Congratulations! Player ${winner} has won.`;
+        messageEl.textContent = `Congratulations! Player  has won.`;
 
     }
 }
 
 
-function render() {
-    updateBoard()
-    updateMessage()
-}
+
 
 
 
